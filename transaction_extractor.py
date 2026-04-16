a0a5a48b7eb4d1d858ee67f93340d3089693e1c4
Chitresh62001 <chitreshimpmails@gmail.com>
2026-04-16 16:13:17 +0530

    updated transaction extractor

--- transaction_extractor.py
+++ transaction_extractor.py
@@ -21,7 +21,11 @@ def extract_transaction_details(text: str,intent : str) -> Dict[str, Optional[st
     # Amount (common)
     # -------------------------
     amount = None
-    m = re.search(r"rs\.?\s*([\d,]+(?:\.\d{1,2})?)", text)
+    if intent != 'INVESTMENT_DEBIT':
+        m = re.search(r"rs.?\s*([\d,]+(?:\.\d{1,2})?)", text)
+    else:
+        m = None
+
     if m:
         amount = float(m.group(1).replace(",", ""))
 
@@ -31,7 +35,7 @@ def extract_transaction_details(text: str,intent : str) -> Dict[str, Optional[st
     # -------------------------
     if intent == 'UPI_DEBIT':
         upi_match = re.search(
-            r"to\s+[a-z0-9.\-_]+@[a-z]+\s+([A-Z ]+?)\s+on\s+\d{1,2}",
+            r"to\s+[a-z0-9.\-_]+@[a-z]+\s+([A-Za-z ]+?)\s+on\s+\d{1,2}",
             original_text
         )
 
@@ -50,7 +54,7 @@ def extract_transaction_details(text: str,intent : str) -> Dict[str, Optional[st
     # -------------------------
     if intent == 'CC_SPEND':
         cc_match = re.search(
-            r"towards\s+([A-Z0-9 &._-]+?)\s+on\s+\d{1,2}",
+            r"towards\s+([A-Za-z0-9 &._-]+?)\s+on\s+\d{1,2}",
             original_text
         )
 
@@ -64,35 +68,37 @@ def extract_transaction_details(text: str,intent : str) -> Dict[str, Optional[st
             }
         
     if intent == 'INVESTMENT_DEBIT':
-        bank_match = re.search(
-        r"towards\s+([A-Z0-9 &._/-]+?)\s+with\s+UMRN",
-        original_text
-    )
+        clean_text = re.sub(r"\s+", " ", original_text)
+        
+        # extract numbers (amount, units, nav)
+        numbers = re.findall(r"\d+\.\d+", clean_text)
+        
+        if len(numbers) >= 3:
+            amount = float(numbers[0])
 
-        if bank_match:
-            name = bank_match.group(1)
+            # fund name = text before amount
+            fund_match = re.search(
+                r"NAV\s+([A-Za-z0-9 \-]+?)\s+" + re.escape(numbers[0]),
+                clean_text
+            )
 
-            # cleanup (remove trailing codes)
-            name = name.split("/")[0].strip()
+            fund_name = fund_match.group(1).strip() if fund_match else None
+
             return {
                 "amount": amount,
-                "counterparty": name,
-                "mode": "BANK"
+                "counterparty": fund_name,
+                "mode": "INVESTMENT",
             }
         
-
     if intent == 'BANK_CREDIT':
-        bank_match = re.search(r"by\s+([a-zA-Z]+)\s+[a-zA-Z0-9.\-_]+@[a-zA-Z]+\s+([A-Z ]+?)\s+on\s+\d{2}-\d{2}-\d{2}",
-                               original_text)
-        print(bank_match)
+        bank_match = re.search(
+            r"by\s+([a-zA-Z]+)\s+[a-zA-Z0-9.\-_]+@[a-zA-Z]+\s+([A-Z ]+?)\s+on\s+\d{2}-\d{2}-\d{2}",
+            original_text
+        )
         if bank_match:
             counterparty = bank_match.group(2)
-            print('THIS IS FIRST COUNTERPARTY ',counterparty)
             mode = bank_match.group(1)
-            print('THIS IS MODE',mode)
             counterparty = re.sub(r"\s+"," ",counterparty).strip()
-            print('THIS IS LAST COUNTERPARTY ',counterparty)
-            
             return {
                 "amount":amount,
                 "counterparty":counterparty,
@@ -110,7 +116,6 @@ def extract_transaction_details(text: str,intent : str) -> Dict[str, Optional[st
 
 
 if __name__ == '__main__':
-    text = """ DEAR CUSTOMER, RS. 25.00 is succesfully credited to your account 
-    **37777 by VPA abc.xyz-2@okicici CHITRESH VIKAS CHOPKAR on 13-04-26
+    text = """ Fund name SIP amount Units credited NAV Nippon India Growth Mid Cap Fund - Direct Plan - Growth Mid Cap 799.96 0.174 4603.6735 NEXT STEPS: Order Placed:  Order placed by you on our platform Order Processed by the Exchange:  This will take up to one working day Mutual Funds Units Credited:  This takes up to T+3 (Trade + 3 working days) Important next step: For the next SIP amount to be deducted automatically from your bank account, complete the mandate process (if you haven?t already) by following these steps: How to automate Mutual Fund SIPs?  Thank you for choosing us for your investment needs! Happy Investing, Team Upstox Follow us on: Available on: Copyright © 2025 Upstox, All rights reserved. Sunshine Tower, 30th Floor, Senapati Bapat Marg, Dadar West, Mumbai 400013, Maharashtra, India.  The information in this email is only for consumption by the client and such material should not be redistributed. About Us  |   Help Center  |  Disclosures and Disclaimer  | Don't like these emails?  Unsubscribe
     """
-    extract_transaction_details(text=text ,intent= 'BANK_CREDIT')
+    extract_transaction_details(text=text ,intent= 'INVESTMENT_DEBIT')
